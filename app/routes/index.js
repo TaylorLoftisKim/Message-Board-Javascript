9a7be95252f7db4cb6bfc7936c9ded392fbc973c
Taylor Loftis-Kim <taylorloftiskim@gmail.com>
2017-04-28 16:30:55 -0700

    Successfully loads Users and Questions from database.

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,21 +1,7 @@
 import Ember from 'ember';
 
-var messages = [{
-  id: 1,
-  user: "Timmy",
-  question: "How to install Ember?"
-}, {
-  id: 2,
-  user: "Megan",
-  question: "What is html?"
-}, {
-  id: 3,
-  user: "Brian",
-  question: "How do I install packages on Atom?"
-}];
-
 export default Ember.Route.extend({
   model() {
-    return messages;
+    return this.store.findAll('message');
   },
 });
